e82ab4c8c4eb27fcdf6cd240d36d8894202b79b1
Coverkk <coverkk@outlook.com>
2022-10-13 09:53:43 +0800

    项目初始化-调整初始目录结构

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
+// vue脚手架项目 - 默认的配置文件名
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // lintOnSave : false,
+  devServer: {
+    port: 80 // 设置端口号
+    // open : true // 浏览器默认打开
+  }
 })
