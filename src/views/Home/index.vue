a468f503d423dc838c6189ffc25026dd8d4491e0
Coverkk <coverkk@outlook.com>
2022-10-14 16:54:13 +0800

    头部导航栏，使用tab标签页

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="home-container">
-    <van-nav-bar class="page-nav-bar" >
-      <van-button slot="title"
-      type="info"
-      size="small"
-      round
-      icon="search"
-      class="search-btn">
+    <!-- navbar导航 - 搜索 -->
+    <van-nav-bar class="page-nav-bar">
+      <van-button slot="title" type="info" size="small" round icon="search" class="search-btn">
         搜索
       </van-button>
     </van-nav-bar>
+    <!-- navbar导航 - 搜索 -->
+
+    <!-- 频道列表 -->
+    <!--
+      通过 v-model 绑定当前激活标签对应的索引值，默认情况下启用第一个标签。
+      通过 animated 属性可以开启切换标签内容时的转场动画。
+      通过 swipeable 属性可以开启滑动切换标签页。
+     -->
+    <van-tabs v-model="active" animated swipeable>
+      <van-tab title="标签 1">内容 1</van-tab>
+      <van-tab title="标签 2">内容 2</van-tab>
+      <van-tab title="标签 3">内容 3</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+      <van-tab title="标签 4">内容 4</van-tab>
+    </van-tabs>
+    <!-- 频道列表 -->
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      active: 0
+    }
+  }
 }
 </script>
 
@@ -27,6 +44,7 @@ export default {
     background-color: #5babfb;
     border: none;
     font-size: 28px;
+
     .van-icon {
       font-size: 32px;
     }
