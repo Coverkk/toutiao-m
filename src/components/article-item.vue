673b9ea625715c3e5ea5b2d680fbf896913930b0
Coverkk <coverkk@outlook.com>
2022-10-15 10:45:02 +0800

    文章列表项，展示列表项内容

--- src/components/article-item.vue
+++ src/components/article-item.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    <van-cell :title="article.title" />
-  </div>
+  <van-cell class="article-item">
+    <template #title>
+      <div class="title">
+        {{ article.title }}
+      </div>
+    </template>
+    <template #label>
+      <div v-if="article.cover.type === 3">
+        <div class="img-item" v-for="(img, index) in article.cover.images" :key="index">
+          <van-image
+          width="100"
+          height="100"
+          :src="img"
+          />
+        </div>
+      </div>
+      <div class="label">
+        <span>{{ article.aut_name }}</span>
+        <span>{{ article.comm_count }}</span>
+        <span>{{ article.pubdate }}</span>
+      </div>
+    </template>
+    <template #default v-if="article.cover.type === 1">
+      <van-image
+      width="100"
+      height="100"
+      :src="article.cover.images[0]"
+      />
+    </template>
+  </van-cell>
 </template>
 
 <script>
