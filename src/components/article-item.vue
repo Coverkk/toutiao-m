9b23381b02d4ac9cf461847d16aa6a405b7e5f7c
Coverkk <coverkk@outlook.com>
2022-10-15 11:02:26 +0800

    文章列表项，样式调整

--- src/components/article-item.vue
+++ src/components/article-item.vue
@@ -1,21 +1,21 @@
 <template>
   <van-cell class="article-item">
     <template #title>
-      <div class="title">
+      <div class="title van-multi-ellipsis--l2">
         {{ article.title }}
       </div>
     </template>
     <template #label>
-      <div v-if="article.cover.type === 3">
+      <div v-if="article.cover.type === 3" class="cover-wrap">
         <div class="img-item" v-for="(img, index) in article.cover.images" :key="index">
           <van-image
-          width="100"
-          height="100"
+          fit="cover"
           :src="img"
+          class="cover-item-img"
           />
         </div>
       </div>
-      <div class="label">
+      <div class="label-info-wrap">
         <span>{{ article.aut_name }}</span>
         <span>{{ article.comm_count }}</span>
         <span>{{ article.pubdate }}</span>
@@ -23,8 +23,8 @@
     </template>
     <template #default v-if="article.cover.type === 1">
       <van-image
-      width="100"
-      height="100"
+      class="right-cover"
+      fit="cover"
       :src="article.cover.images[0]"
       />
     </template>
@@ -42,6 +42,41 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped lang="less">
+.article-item {
+    .title {
+        font-size: 32px;
+        color: #3a3a3a;
+    }
+    .van-cell__value {
+        flex: unset;
+        width: 232px;
+        height: 146px;
+        padding-left: 25px;
+        .right-cover {
+          width: 232px;
+          height: 146px;
+        }
+    }
+    .label-info-wrap span {
+        font-size: 22px;
+        color: #b4b4b4;
+        margin-right: 25px;
+    }
+    .cover-wrap {
+        display: flex;
+        padding: 30px 0;
+        .img-item {
+            flex: 1;
+            height: 146px;
+            &:not(:last-child) {
+                padding-right: 4px;
+            }
+            .cover-item-img {
+                width: 100%;
+                height: 146px;
+            }
+        }
+    }
+}
 </style>
