f614add7d58e5ab6c495ec0328e324304bcbdcfa
Coverkk <coverkk@outlook.com>
2022-10-17 16:59:26 +0800

    文章搜索，处理页面展示逻辑

--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -3,27 +3,29 @@
     <!-- 顶部搜索栏 -->
     <form action="/">
         <van-search
-            v-model="value"
+            v-model="searchText"
             show-action
             background="#3296fa"
             placeholder="请输入搜索关键词"
             @search="onSearch"
             @cancel="onCancel"
+            @focus="isResultShow = false"
         />
     </form>
     <!-- 顶部搜索栏 -->
 
-    <!-- 搜索历史记录 -->
-    <SearchHistory></SearchHistory>
-    <!-- 搜索历史记录 -->
+    <!-- 搜索结果 -->
+    <SearchResult v-if="isResultShow"></SearchResult>
+    <!-- 搜索结果 -->
 
     <!-- 联想建议 -->
-    <SearchSuggestion></SearchSuggestion>
+    <SearchSuggestion v-else-if="searchText.trim()"></SearchSuggestion>
     <!-- 联想建议 -->
 
-    <!-- 搜索结果 -->
-    <SearchResult></SearchResult>
-    <!-- 搜索结果 -->
+    <!-- 搜索历史记录 -->
+    <SearchHistory v-else></SearchHistory>
+    <!-- 搜索历史记录 -->
+
   </div>
 </template>
 
@@ -34,12 +36,13 @@ import SearchResult from './components/search-result.vue'
 export default {
   data () {
     return {
-      value: ''
+      searchText: '',
+      isResultShow: false // 控制搜索结果展示
     }
   },
   methods: {
     onSearch (val) {
-      console.log(val)
+      this.isResultShow = true
     },
     onCancel () {
       this.$router.back()
