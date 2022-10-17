058ef875d8e9a2b3bffd381b2328c7de3c834f51
Coverkk <coverkk@outlook.com>
2022-10-17 16:36:03 +0800

    文章搜索，页面布局完成

---
+++ src/views/Search/components/search-suggestion.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-suggestion">
+    <van-cell title="kkkkk" icon="search"></van-cell>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+
+</style>
--- src/views/Search/index.vue
+++ src/views/Search/index.vue
@@ -1,13 +1,62 @@
 <template>
-  <div class="search-container">搜索</div>
+  <div class="search-container">
+    <!-- 顶部搜索栏 -->
+    <form action="/">
+        <van-search
+            v-model="value"
+            show-action
+            background="#3296fa"
+            placeholder="请输入搜索关键词"
+            @search="onSearch"
+            @cancel="onCancel"
+        />
+    </form>
+    <!-- 顶部搜索栏 -->
+
+    <!-- 搜索历史记录 -->
+    <SearchHistory></SearchHistory>
+    <!-- 搜索历史记录 -->
+
+    <!-- 联想建议 -->
+    <SearchSuggestion></SearchSuggestion>
+    <!-- 联想建议 -->
+
+    <!-- 搜索结果 -->
+    <SearchResult></SearchResult>
+    <!-- 搜索结果 -->
+  </div>
 </template>
 
 <script>
+import SearchHistory from './components/search-history.vue'
+import SearchSuggestion from './components/search-suggestion.vue'
+import SearchResult from './components/search-result.vue'
 export default {
-
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    onSearch (val) {
+      console.log(val)
+    },
+    onCancel () {
+      this.$router.back()
+    }
+  },
+  components: {
+    SearchHistory,
+    SearchSuggestion,
+    SearchResult
+  }
 }
 </script>
 
 <style scoped lang="less">
-
+.search-container {
+    .van-search__action {
+        color: #fff;
+    }
+}
 </style>
