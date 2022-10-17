1bc87678a2bf8fb11910174cf63d9f8d539036c9
Coverkk <coverkk@outlook.com>
2022-10-17 19:05:52 +0800

    文章搜索，搜索防抖

--- src/views/Search/components/search-suggestion.vue
+++ src/views/Search/components/search-suggestion.vue
@@ -6,11 +6,12 @@
 
 <script>
 import { searchSuggestionAPI } from '@/api'
+import { debounce } from 'lodash'
 export default {
   data () {
     return {
-      suggestions: [], // 联想建议数据列表
-      timer: null // 联想建议防抖定时器
+      suggestions: [] // 联想建议数据列表
+      // timer: null // 联想建议防抖定时器
     }
   },
   props: {
@@ -22,13 +23,22 @@ export default {
   watch: {
     // 侦听输入框输入内容searchText的变化，当searchText变化时，调用API接口，获取联想建议
     searchText: {
-      handler (val) {
-        // console.log(val)
-        if (val.trim()) {
-        //   console.log(val)
-          this.searchSuggestion()
-        }
-      },
+    //   handler (val) {
+    //     // console.log(val)
+    //     if (val.trim()) {
+    //     //   console.log(val)
+    //       this.searchSuggestion()
+    //     }
+    //   },
+    // _.debounce(func, [wait=0], [options=])
+    // 创建一个 debounced（防抖动）函数，该函数会从上一次被调用后，延迟 wait 毫秒后调用 func 方法。
+    // debounced（防抖动）函数提供一个 cancel 方法取消延迟的函数调用以及 flush 方法立即调用。
+    // 可以提供一个 options（选项） 对象决定如何调用 func 方法，options.leading 与|或 options.trailing 决定延迟前后如何触发
+    // （注：是 先调用后等待 还是 先等待后调用）。
+    // 优化防抖
+      handler: debounce(function () {
+        this.searchSuggestion()
+      }, 200),
       immediate: true
     }
   },
@@ -36,19 +46,28 @@ export default {
     // 获取联想建议
     async searchSuggestion () {
       // 如果有定时器，删除上一个定时器
-      if (this.timer) {
-        clearTimeout(this.timer)
+      //   优化防抖
+    //   if (this.timer) {
+    //     clearTimeout(this.timer)
+    //   }
+    //   this.timer = setTimeout(async () => {
+    //     try {
+    //       const { data: { data: { options } } } = await searchSuggestionAPI(this.searchText)
+    //       // console.log(res)
+    //       // 将获取到的结果放到联想建议的数组中
+    //       this.suggestions = options
+    //     } catch (err) {
+    //       this.$toast('获取数据失败，请稍后重试')
+    //     }
+    //   }, 500)
+      try {
+        const { data: { data: { options } } } = await searchSuggestionAPI(this.searchText)
+        // console.log(res)
+        // 将获取到的结果放到联想建议的数组中
+        this.suggestions = options
+      } catch (err) {
+        this.$toast('获取数据失败，请稍后重试')
       }
-      this.timer = setTimeout(async () => {
-        try {
-          const { data: { data: { options } } } = await searchSuggestionAPI(this.searchText)
-          // console.log(res)
-          // 将获取到的结果放到联想建议的数组中
-          this.suggestions = options
-        } catch (err) {
-          this.$toast('获取数据失败，请稍后重试')
-        }
-      }, 500)
     }
   }
 }
